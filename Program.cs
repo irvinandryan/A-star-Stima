632f936453d73e3e680077096bf868f69a374ae7
rafirm29 <rafiray.munandar@gmail.com>
2021-04-06 23:16:06 +0700

    AStar done in csharp files

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AStar;
+using System.Collections.Generic;
+
+namespace AStarStima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new Graph();
+            g.readFromFile("input.txt");
+            //g.printGraph();
+
+            foreach (var item in g.nodeList)
+            {
+                item.neighborInfo();
+            }
+
+            Console.WriteLine("\n----------------\n");
+
+            Node path = g.AStar(g.findNode("Arad"), g.findNode("Bucharest"));
+            if (path != null)
+            {
+                g.printPath(path);
+            }
+            else
+            {
+                Console.WriteLine("Ga ketemu");
+            }
+        }
+    }
+}
