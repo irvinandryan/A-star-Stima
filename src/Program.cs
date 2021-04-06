9ba4ff1201ba5f515a05da19d5b92463e1af5c26
rafirm29 <rafiray.munandar@gmail.com>
2021-04-07 02:36:37 +0700

    final

---
+++ src/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using AStar;
+using System.Collections.Generic;
+
+namespace AStarStima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new Graph();
+
+            bool invalidFileName = true;
+            while (invalidFileName)
+            {
+                Console.Write("Silakan masukkan nama file: ");
+                string input = Console.ReadLine();
+                try
+                {
+                    g.readFromFile($"{input}");
+                    invalidFileName = false; ;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Nama file tidak ditemukan!");
+                }
+            }
+
+            g.printNodeList();
+            Node start = null;
+            Node target = null;
+
+            while (start == null)
+            {
+                Console.Write("Silakan masukkan start node: ");
+                string input = Console.ReadLine();
+                start = g.findNode(input);
+                if (start == null)
+                {
+                    Console.WriteLine("Input invalid!");
+                }
+            }
+            while (target == null)
+            {
+                Console.Write("Silakan masukkan target node: ");
+                string input = Console.ReadLine();
+                target = g.findNode(input);
+                if (target == null)
+                {
+                    Console.WriteLine("Input invalid!");
+                }
+            }
+
+            g.printGraph();
+
+            Node path = g.AStar(start, target);
+            if (path != null)
+            {
+                g.printPath(path);
+            }
+            else
+            {
+                Console.WriteLine("Ga ketemu");
+            }
+        }
+    }
+}
